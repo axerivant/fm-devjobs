87f8cca60de83adae04d09eebf0e9a8ec41447b8
Axer Ivant <axerivant@icloud.com>
2023-01-06 09:42:50 -0800

    Card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { Card } from '@/ui/base'
 	import { Button } from '@/ui/interactive'
 </script>
 
-<div class="flex flex-col justify-center items-center p-36 gap-6">
-	<Button label="Button A" />
-	<Button label="Button B" type="secondary" />
+<div class="flex flex-col justify-center items-center p-36 bg-gray-700">
+	<Card flex="col" tw="justify-center items-center">
+		<Button label="Button A" />
+		<Button label="Button B" type="secondary" />
+	</Card>
 </div>
---
+++ src/ui/base/Card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Theme from '@/global/theme'
+
+	export let width: string = ''
+	export let flex: 'row' | 'col' | '' = ''
+	export let tw: string = ''
+</script>
+
+<div
+	class="rounded p-3 gap-3 {$Theme === 'light'
+		? 'bg-white'
+		: 'bg-gray-900'} {flex && 'flex'} {flex === 'col'
+		? 'flex-col'
+		: 'flex-row'} {tw}"
+>
+	<slot />
+</div>
