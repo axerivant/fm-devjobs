636d1e41623124263a9c1f6512f680e2d62566c0
Axer Ivant <axerivant@icloud.com>
2023-01-06 09:27:31 -0800

    Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <script lang="ts">
-	import { Header } from '@/ui/common'
+	import { Button } from '@/ui/interactive'
 </script>
+
+<div class="flex flex-col justify-center items-center p-36 gap-6">
+	<Button label="Button A" />
+	<Button label="Button B" type="secondary" />
+</div>
---
+++ src/ui/interactive/Button.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Theme from '@/global/theme'
+
+	export let label: string
+	export let type: 'primary' | 'secondary' = 'primary'
+</script>
+
+{#if type === 'primary'}
+	<button
+		class="bg-indigo-500 text-white hover:opacity-80 transition-all"
+		on:click
+	>
+		{label}
+	</button>
+{/if}
+
+{#if type === 'secondary'}
+	<button
+		class="
+            {$Theme === 'light'
+			? 'text-indigo-500 bg-indigo-100 hover:bg-indigo-200'
+			: 'text-white bg-gray-600 hover:bg-gray-500'} 
+            transition-all"
+		on:click
+	>
+		{label}
+	</button>
+{/if}
+
+<style>
+	button {
+		@apply font-semibold py-3 px-6 flex justify-center items-center rounded;
+	}
+</style>
