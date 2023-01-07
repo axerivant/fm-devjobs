a11b601ee149279748e80d1610142a21f142a89b
Axer Ivant <axerivant@icloud.com>
2023-01-07 02:19:25 -0800

    Added Background

---
+++ src/ui/common/Background.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Theme from '@/global/theme'
+</script>
+
+<div
+	class=" h-screen w-screen fixed top-0 left-0 -z-30 {$Theme === 'light'
+		? 'bg-indigo-100'
+		: 'bg-gray-900'}"
+/>
