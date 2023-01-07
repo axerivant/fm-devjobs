c1270fd7bc43eef647d1d765f73d016329293d2e
Axer Ivant <axerivant@icloud.com>
2023-01-07 02:35:28 -0800

    - added transition to Background

--- src/ui/common/Background.svelte
+++ src/ui/common/Background.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <div
-	class=" h-screen w-screen fixed top-0 left-0 -z-30 {$Theme === 'light'
+	class=" h-screen w-screen fixed top-0 left-0 -z-30 transition-all {$Theme ===
+	'light'
 		? 'bg-indigo-100'
 		: 'bg-gray-900'}"
 />
